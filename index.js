6181a5fd9e282cf999037e755a4be7f209ddad09
Grace Song <143944107+gracesong617@users.noreply.github.com>
2023-12-01 17:34:37 +0800

    2 players

---
+++ index.js
@@ -0,0 +1,135 @@
+//creating the express app
+let express= require("express");
+let app = express();
+app.use("/", express.static("public"));
+
+//creating the http server - this is a new step!
+let http = require("http");
+let server = http.createServer(app);
+const PORT = process.env.PORT || 9999;
+server.listen(PORT, () => {
+    console.log("server on port ",PORT);
+});
+
+let roomCodeList = [];
+let gamePlayer = 0;
+  
+
+//initialise socket.io
+let io = require("socket.io")(server);
+
+//listen for a new connection
+io.sockets.on("connection", (socket) => {
+  console.log("new connection :", socket.id);
+
+  if (roomCodeList.length > 2 ){
+    return;
+  }
+  //create room
+  socket.on("login", (playerName) => {
+    console.log(playerName);
+    const roomCode = Math.floor(Math.random() * 10000);
+    roomCodeList.push(roomCode);
+    socket.join(roomCode);
+
+    // Print the room code to the server console
+    console.log("Room code created:", roomCode);
+    
+    // Send the room code to all clients
+     io.emit("loginSuccess", roomCode);
+  })
+
+  //incase of disconnection
+  socket.on("disconnect", function() {
+    console.log("disconnection :", socket.id)
+    roomCodeList = [];
+    gamePlayer = 0;
+    socket.broadcast.emit("refresh");
+  });
+
+  socket.on("createRoom", function(roomCode){
+    //check roomcode
+    //if roomcode not exist
+    if (roomCodeList.indexOf(parseInt(roomCode)) != -1) {
+        //add roomcode
+        roomCodeList.push(roomCode);
+
+        //send add success
+        socket.emit("addSuccess");
+
+        //add player number
+        gamePlayer++;
+
+        //check player number
+        if (gamePlayer == 1 ){
+            socket.position = "1p";
+            socket.join(roomCode);
+            const playerID = "1p";
+            io.to(socket.id).emit("playerID", playerID);    
+            console.log("player joined:",playerID);
+
+        } else if (gamePlayer == 2) {
+            socket.position = "2p";
+            socket.join(roomCode);
+            const playerID = "2p";
+            io.to(socket.id).emit("playerID", playerID);
+            console.log("player joined:",playerID);
+        } else {
+            socket.position = "wrong";
+        }
+        if (gamePlayer == 2) {
+            console.log("Reached enough players");
+            socket.emit("gameStart",socket.position)
+            socket.broadcast.emit("GameStart","1p");
+        }else {
+            socket.position = "wrong";
+        }
+    }
+  });
+
+  socket.on("position1", function (position1) {
+    socket.broadcast.emit("position1Fresh", position1);
+    if (position1 <= 50) {
+        var result = "1p";
+        socket.emit("gameOver", result);
+        socket.broadcast.emit("gameOver", result);
+    }
+  
+  });
+  
+  socket.on("position2", function (position2) {
+    socket.broadcast.emit("position2Fresh", position2);
+    if (position2 <= 50) {
+        var result = "2p";
+        socket.broadcast.emit("gameOver", result);
+        socket.emit("gameOver", result);
+    }
+  });
+
+});
+
+
+
+
+
+//game
+
+
+
+//1
+//setting up an http server
+//setting up socket.io
+
+//2
+//ensure that the client can connect to the server via sockets
+//server recognising the connect
+//client attempting to connect
+
+//3
+//client draws and sends to server
+
+//4
+//server recieves and sends to all the clients
+
+//5
+//clients recieve and draw on their screens
---
+++ public/app.js
@@ -0,0 +1,189 @@
+
+let socket = io.connect();
+let playerID;
+
+document.addEventListener("DOMContentLoaded", function () {
+    var login = new Login();
+    login.init();
+});
+
+function Login() {
+    this.socket = io.connect();
+}
+
+Login.prototype.init = function () {
+    var self = this;
+    
+    self.socket.on("connect", function () {
+        var noticeElement = document.querySelector(".result .notice");
+        if (noticeElement) {
+            noticeElement.innerHTML = "";
+        } else {
+            console.error("Element with selector '.result .notice' not found.");
+        }
+
+        var textElement = document.querySelector(".result .connect");
+        if (textElement) {
+            textElement.textContent = "Connect Success!";
+        } else {
+            console.error("Element with selector '.result .connect' not found.");
+        }
+    });
+
+    // create new room
+    document.getElementById("build-button").addEventListener("click", function () {
+        var buildTextValue = document.getElementById("build-button").value;
+
+            var playerName = buildTextValue;
+            self.socket.emit("login",playerName);
+    
+    });
+    
+    // get room code
+    self.socket.on("loginSuccess", function (roomCode) {
+        var textElement = document.querySelector(".result .text");
+        if (textElement) {
+            textElement.textContent = 'Your room code is ' + roomCode;
+        } else {
+            console.error("Element with selector '.result .text' not found.");
+        }
+    });
+    
+
+    // use room code to join the game
+    document.getElementById("join-button").addEventListener("click", function () {
+        var joinTextValue = document.getElementById("join-text").value;
+
+        if (joinTextValue.trim().length <= 0) {
+            var noticeElement = document.querySelector(".result .notice");
+            var textElement = document.querySelector(".result .text");
+            if (noticeElement) {
+                noticeElement.textContent = "ERROR";
+            }
+            if (textElement) {
+                textElement.textContent = "RoomCode Wrong";
+            }
+        } else {
+            self.socket.emit("createRoom", joinTextValue);
+        }
+    });
+    self.socket.on("addSuccess", function () {
+        var textElement = document.querySelector(".join .text");
+        if (textElement) {
+          textElement.textContent = "Wait For Another Player";
+          console.log(playerID);
+        }
+    });
+    self.socket.on("playerID", function (receivedPlayerID) {
+        playerID = receivedPlayerID;
+        window.player = playerID;
+        console.log("Received playerID:", playerID);
+
+    });
+
+};
+
+
+//game
+
+let p1, p2, p1Image, p2Image, flashImage, roadImage;
+let p1_X,p2_X;
+let p1_Y =520,p2_Y = 520;
+let ystart = 520;
+let move;
+let isKeyPressed = false;
+
+
+function preload(){
+    p1Image = loadImage('images/p1.png');
+
+    p2Image = loadImage('images/p2.png');
+
+    flashImage = loadImage('images/flash.jpg');
+    roadImage = loadImage('images/road_bg.jpg');
+}
+
+function setup() {
+    createCanvas(800, 600);
+    background("#a5ddbf");
+    socket = io.connect();
+
+    image(roadImage,200,50);
+
+
+    p1Image.resize(p1Image.width * 0.2, p1Image.height * 0.2);
+    p2Image.resize(p2Image.width * 0.2, p2Image.height * 0.2);
+};
+
+
+
+function draw(){
+    clear();
+    background("#a5ddbf");
+
+     // hint
+     textSize(15);
+     fill(0);
+     textFont('DotGothic16');
+     text('press w to move character',10,130);
+
+    image(roadImage,200,50);
+    if (playerID) { 
+        // playerInfo
+        textSize(15);
+        fill(0);
+        textFont('DotGothic16');
+        text(`You are: ${playerID}`, 10, 100);
+    }
+
+      //p1
+      image(p1Image,310,p1_Y);
+      //p2
+      image(p2Image,410,p2_Y);
+
+
+
+    socket.on("position1Fresh", function (position1) {
+        if (playerID == "2p") {
+          p1_Y = position1;
+        }
+    });
+  
+    socket.on("position2Fresh", function (position2) {
+        if (playerID == "1p") {
+          p2_Y = position2;
+        }
+    });
+
+
+    socket.on("refresh", function () {
+        location.reload();
+    });
+
+    socket.on("gameOver", function (result) {
+        alert(result + " is the Winner");
+    });
+}
+
+
+function keyPressed() {
+    if (keyCode === 87 && isKeyPressed === false) {
+        if (window.player == "1p") {
+            p1_Y -= 10;
+            socket.emit("position1", p1_Y);
+        } else if (window.player == "2p") {
+            p2_Y -= 10;
+            socket.emit("position2", p2_Y);
+        }
+        isKeyPressed = true;
+    }
+}
+
+function keyReleased() {
+    if (keyCode === 87) {
+        isKeyPressed = false;
+    }
+}
+
+
+
